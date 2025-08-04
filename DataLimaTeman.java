79b2fe2dc3e5d825e7e395ced80e240c20c0261b
SaktiZaman <sakti_zaman_34rpl@student.smktelkom-mlg.sch.id>
2025-08-04 14:23:26 +0700

    Add files via upload

---
+++ DataLimaTeman.java
@@ -0,0 +1,30 @@
+public class DataLimaTeman {
+    public static void main(String[] args) {
+        String namaMaven = "Maven Helios Agathon Yesstian";
+        String namaAdis = "Radisty Kirana Dhisa Aqila";
+        String namaLintang = "Intazzar Lintang Pramanta";
+        String namaAtha = "Atha Khoirun Ayyasi";
+        String namaJeri = "Muhammad Jericho Husein";
+
+        Integer usiaMaven = 15;
+        Integer usiaAdis = 15;
+        Integer usiaLintang = 16;
+        Integer usiaAtha = 15;
+        Integer usiaJeri = 15;
+
+        
+        String citaMaven = "Web Developer";
+        String citaAdis = "software Engineer";
+        String citaLintang = "it developer";
+        String citaAtha = "pengusaha";
+        String citaJeri = "menjadi milliarder";
+
+
+        System.out.println(namaAdis + " " + usiaAdis + " " + citaAdis);
+        System.out.println(namaAtha + " " + usiaAtha + " " + citaAtha);
+        System.out.println(namaJeri + " " + usiaJeri + " " + citaJeri);
+        System.out.println(namaLintang + " " + usiaLintang + " " + citaLintang);
+        System.out.println(namaMaven + " " + usiaMaven + " " + citaMaven);
+
+    }
+}
