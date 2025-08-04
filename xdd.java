33a7da91a9722e05a8e8210b7ffd950d25cc1bbd
SaktiZaman <sakti_zaman_34rpl@student.smktelkom-mlg.sch.id>
2025-08-04 13:16:49 +0700

    Add files via upload

--- xdd.java
+++ xdd.java
@@ -1,5 +1,11 @@
 public class xdd {
     public static void main(String[] args) {
         System.out.println("Sakti mz");
+        System.out.println("Smk Telkom Malang");
+        System.out.println("PKBM Piwulang Becik");
+        System.out.println("Mokhamad Badrus Zaman");
+        System.out.println("Erika Dinamika mardianus");
+        System.out.println("sukses, membahagiakan orang tua");
+        System.out.println("nonton film");
     }
 }
